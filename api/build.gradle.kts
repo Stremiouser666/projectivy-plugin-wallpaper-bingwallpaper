7bcb3538264f772f907ad704525ef7704a38a480
Steveies <protonhollis@gmail.com>
2025-12-24 05:46:44 +0000

    Update build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,47 +1,26 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
 }
 
 android {
     namespace = "tv.projectivy.plugin.wallpaperprovider.api"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
-        minSdk = 23
-
-        consumerProguardFiles("consumer-rules.pro")
+        minSdk = 21
     }
-
+    
     buildFeatures {
         aidl = true
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
+    
+    kotlinOptions {
+        jvmTarget = "17"
     }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
 }
