d7229ea1045ca7ac1e782919f7dabb18610fab42
Steveies <protonhollis@gmail.com>
2025-12-24 00:08:40 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,1 @@
-plugins {
-    id("com.android.library") version "8.12.2" apply false
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "tv.projectivy.plugin.wallpaperprovider.bingwallpaper"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-}
-
-dependencies {
-    // Projectivy API dependency (if used)
-}
+// Root project build file
